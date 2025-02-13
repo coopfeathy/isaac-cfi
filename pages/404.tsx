1de411b282b414eb198d4dd042febd87956e6ebc
Cooper Featherstone <cooperfeatherstone13@gmail.com>
2025-02-13 13:37:00 -0600

    test2

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,7 +5,7 @@ export default function Custom404() {
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-6xl font-bold">404</h1>
       <h2 className="text-2xl mt-3 mb-5">Page Not Found</h2>
-      <p className="mb-5">The page you're looking for doesn't exist or has been moved.</p>
+      <p className="mb-5">The page you&apos;re looking for doesn&apos;t exist or has been moved.</p>
       <Link href="/" className="text-blue-500 hover:text-blue-700">
         Go back home
       </Link>
