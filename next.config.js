d7457de0323aca06e3bd8a64068adf65354a4051
Cooper Featherstone <cooperfeatherstone13@gmail.com>
2025-11-18 17:22:03 -0600

    Add Google Calendar env vars and fix webpack config for Netlify

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['googleapis'],
+  },
+  webpack: (config) => {
+    // Ignore optional dependencies that cause warnings
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'supports-color': false,
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig
